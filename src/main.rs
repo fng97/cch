ff6ea57d985313b2327e8bbca46dce9712c4f664
Francisco <53615823+fng97@users.noreply.github.com>
2023-11-26 19:36:10 +0000

    Adds health_check endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -6,10 +6,15 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(hello_world).service(health_check);
     };
 
     Ok(config.into())
