1e9f4d9d6391dab2975214d222d178abc4865d32
Francisco <53615823+fng97@users.noreply.github.com>
2023-11-26 20:19:45 +0000

    Removes hello_world root handler.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
-
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -14,7 +9,7 @@ async fn health_check() -> impl Responder {
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world).service(health_check);
+        cfg.service(health_check);
     };
 
     Ok(config.into())
