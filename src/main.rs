5017ad5bd91699f8405fda99806056ff12581d6f
Francisco <53615823+fng97@users.noreply.github.com>
2023-11-26 20:21:50 +0000

    Splits project into library and executable.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use fng_cch::run;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(health_check);
-    };
-
-    Ok(config.into())
+    run().await
 }
