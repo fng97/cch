d40bbc364be59234e72cdf3ae0a9ef118a1f1a87
Francisco <53615823+fng97@users.noreply.github.com>
2023-11-28 19:50:50 +0000

    Move spawn_app testing logic into helpers module.

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,20 +1,4 @@
-use actix_web::{App, HttpServer};
-use std::net::TcpListener;
-
-use cch::config;
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = HttpServer::new(|| App::new().configure(config))
-        .listen(listener)
-        .expect("Failed to bind TCP listener.")
-        .run();
-
-    tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
-}
+use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn health_check_works() {
---
+++ tests/api/helpers.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpServer};
+use std::net::TcpListener;
+
+use cch::startup::config;
+
+pub fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = HttpServer::new(|| App::new().configure(config))
+        .listen(listener)
+        .expect("Failed to bind TCP listener.")
+        .run();
+
+    tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
