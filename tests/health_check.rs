a308b27f90829e7bf0b95c13bf3b1714639f4ff0
Francisco <53615823+fng97@users.noreply.github.com>
2023-11-28 19:15:50 +0000

    Refactors testing app spawner to use dynamic port.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,26 +1,30 @@
 use actix_web::{App, HttpServer};
+use std::net::TcpListener;
 
 use cch::config;
 
-fn spawn_app() -> std::io::Result<()> {
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
     let server = HttpServer::new(|| App::new().configure(config))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)
+        .expect("Failed to bind TCP listener.")
         .run();
 
     tokio::spawn(server);
 
-    Ok(())
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app().expect("Failed to spawn our app.");
+    let app_address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &app_address))
         .send()
         .await
         .expect("Failed to execute request.");
