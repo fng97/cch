f347c31cb13e9ca154e44f74b9bcd1dd832b0bc1
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-01 23:35:34 +0000

    Dropping validation and allowing up to i128.
    Bonus task isn't passing in CCH.

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -7,17 +7,11 @@ use actix_web::{get, web, Either, HttpResponse, Result};
 async fn cube_xor_of_path_params(path: web::Path<String>) -> Either<HttpResponse, Result<String>> {
     let params = path.into_inner();
     let params = params.split('/').collect::<Vec<&str>>();
-    let params = params.iter().map(|s| s.trim()).collect::<Vec<&str>>();
-
-    // check number of params is between 1 and 20
-    if params.is_empty() || params.len() > 20 {
-        return Either::Left(HttpResponse::BadRequest().body("Bad Request"));
-    }
 
     let mut result = 0;
 
     for number in params {
-        match number.parse::<u32>() {
+        match number.parse::<i128>() {
             Ok(n) => result = result.bitxor(n),
             Err(_) => return Either::Left(HttpResponse::BadRequest().body("Bad Request")),
         }
