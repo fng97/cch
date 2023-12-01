f9547b72188d7cdb217ada0c54e4076677f6a3ae
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-01 18:38:01 +0000

    Removes health_check endpoint.

--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,5 @@
 use actix_web::{get, HttpResponse, Responder};
 
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
 #[get("/")]
 async fn root() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,8 @@
 use actix_web::web::ServiceConfig;
 
 use crate::routes::fake_error;
-use crate::routes::health_check;
 use crate::routes::root;
 
 pub fn config(cfg: &mut ServiceConfig) {
-    cfg.service(root).service(health_check).service(fake_error);
+    cfg.service(root).service(fake_error);
 }
