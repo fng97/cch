23d209b89e924c6d2bc9eece25ac746de76896cc
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-01 21:41:52 +0000

    Updates cube_xor_of_path_params to pass tests.

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -1,14 +1,31 @@
 use std::ops::BitXor;
 
-use actix_web::{get, web, Result};
+use actix_web::{get, web, Either, HttpResponse, Result};
 
-#[get("/1/{x}/{y}")]
-async fn cube_xor_of_path_params(path: web::Path<(u32, u32)>) -> Result<String> {
-    let (x, y) = path.into_inner();
-    let xored = x.bitxor(y);
-    let cubed = xored.pow(3);
+// TODO: Find a better way of validating the input, regex or type?
+#[get("/1/{params:.*}")] // match all path params
+async fn cube_xor_of_path_params(path: web::Path<String>) -> Either<HttpResponse, Result<String>> {
+    let params = path.into_inner();
+    let params = params.split('/').collect::<Vec<&str>>();
+    let params = params.iter().map(|s| s.trim()).collect::<Vec<&str>>();
 
-    Ok(cubed.to_string())
+    // check number of params is between 1 and 20
+    if params.is_empty() || params.len() > 20 {
+        return Either::Left(HttpResponse::BadRequest().body("Bad Request"));
+    }
+
+    let mut result = 0;
+
+    for number in params {
+        match number.parse::<u32>() {
+            Ok(n) => result = result.bitxor(n),
+            Err(_) => return Either::Left(HttpResponse::BadRequest().body("Bad Request")),
+        }
+    }
+
+    let cubed = result.pow(3);
+
+    Either::Right(Ok(cubed.to_string()))
 }
 
 // just checking I've understood the bitxor trait
--- tests/api/day1.rs
+++ tests/api/day1.rs
@@ -1,17 +1,18 @@
 use crate::helpers::spawn_app;
 
+const DAY: u8 = 1;
+
 // Task 1
 #[tokio::test]
-async fn get_1_cubes_xored_path_param_pair() {
+async fn cube_xored_path_params() {
     // Arrange
     let app_address = spawn_app();
     let client = reqwest::Client::new();
-    let challenge_path = format!("{}/1", &app_address);
 
     // Act
     let response = client
         // from example: xor(4, 8)^3 = 1728
-        .get(&format!("{challenge_path}/4/8"))
+        .get(format!("{app_address}/{DAY}/4/8"))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -19,4 +20,38 @@ async fn get_1_cubes_xored_path_param_pair() {
     // Assert
     assert_eq!(200, response.status().as_u16());
     assert_eq!("1728", response.text().await.unwrap());
+}
+
+// Task 2
+#[tokio::test]
+async fn get_1_cubes_xored_path_params() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // example inputs
+    let test_cases = [
+        ("10", 200, "1000"),      // GET /1/10 returns 10
+        ("4/5/8/10", 200, "27"),  // GET /1/4/5/8/10 returns 27
+        ("", 400, "Bad Request"), // no path params
+        (
+            "1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21",
+            400,
+            "Bad Request",
+        ), // too many path params
+    ];
+
+    for (input, expected_status, expected_body) in test_cases {
+        // Act
+        let response = client
+            // from example: xor(4, 8)^3 = 1728
+            .get(format!("{app_address}/{DAY}/{input}"))
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(expected_status, response.status().as_u16());
+        assert_eq!(expected_body, response.text().await.unwrap());
+    }
 }
