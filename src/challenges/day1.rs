1905dbc7807948504b73ca6e3df76aae9cbfa199
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-02 20:27:54 +0000

    Refactored to abstract business logic.
    Handler now just responsible for error handling.

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -1,31 +1,37 @@
+use actix_web::{get, web, Either, HttpResponse};
+use std::num::ParseIntError;
 use std::ops::BitXor;
 
-use actix_web::{get, web, Either, HttpResponse, Result};
+#[get("/1/{tail:.*}")] // match all
+async fn cube_xor_of_path_params(path: web::Path<String>) -> Either<String, HttpResponse> {
+    let ids = match extract_path_params(&path.into_inner()) {
+        Ok(p) => p,
+        Err(_) => return Either::Right(HttpResponse::BadRequest().body("Bad Request")),
+    };
 
-// TODO: Find a better way of validating the input, regex or type?
-#[get("/1/{params:.*}")] // match all path params
-async fn cube_xor_of_path_params(path: web::Path<String>) -> Either<HttpResponse, Result<String>> {
-    let params = path.into_inner();
-    let params = params.split('/').collect::<Vec<&str>>();
+    // check number of ids is between 1 and 20
+    if ids.len() > 20 {
+        return Either::Right(HttpResponse::BadRequest().body("Bad Request"));
+    }
+
+    Either::Left(recallibrate(ids).to_string())
+}
+
+fn extract_path_params(path: &str) -> Result<Vec<i32>, ParseIntError> {
+    let params = path.split('/').collect::<Vec<&str>>();
     let params = params.iter().map(|s| s.trim()).collect::<Vec<&str>>();
 
-    // check number of params is between 1 and 20
-    if params.is_empty() || params.len() > 20 {
-        return Either::Left(HttpResponse::BadRequest().body("Bad Request"));
-    }
+    params.iter().map(|s| s.parse::<i32>()).collect()
+}
 
+fn recallibrate(ids: Vec<i32>) -> i32 {
     let mut result = 0;
 
-    for number in params {
-        match number.parse::<i32>() {
-            Ok(n) => result = result.bitxor(n),
-            Err(_) => return Either::Left(HttpResponse::BadRequest().body("Bad Request")),
-        }
+    for id in ids {
+        result = result.bitxor(id);
     }
 
-    let cubed = result.pow(3);
-
-    Either::Right(Ok(cubed.to_string()))
+    result.pow(3)
 }
 
 // just checking I've understood the bitxor trait
