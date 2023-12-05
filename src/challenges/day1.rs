2ba8b3c22674de1dec331ed4b82fb899f5741454
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-05 23:32:02 +0000

    Moves validation to a type.

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -1,27 +1,60 @@
-use actix_web::{get, web, Either, HttpResponse};
+use actix_web::get;
+use actix_web::{Error, FromRequest, HttpRequest};
+use futures_util::future::{ready, Ready};
 use std::num::ParseIntError;
 use std::ops::BitXor;
+use std::str::FromStr;
 
-#[get("/1/{tail:.*}")] // match all
-async fn cube_xor_of_path_params(path: web::Path<String>) -> Either<String, HttpResponse> {
-    let ids = match extract_path_params(&path.into_inner()) {
-        Ok(p) => p,
-        Err(_) => return Either::Right(HttpResponse::BadRequest().body("Bad Request")),
-    };
-
-    // check number of ids is between 1 and 20
-    if ids.len() > 20 {
-        return Either::Right(HttpResponse::BadRequest().body("Bad Request"));
-    }
-
-    Either::Left(recallibrate(ids).to_string())
+enum PathParamsError {
+    ParseError(ParseIntError),
+    TooManyIds,
 }
 
-fn extract_path_params(path: &str) -> Result<Vec<i32>, ParseIntError> {
-    let params = path.split('/').collect::<Vec<&str>>();
-    let params = params.iter().map(|s| s.trim()).collect::<Vec<&str>>();
+impl From<ParseIntError> for PathParamsError {
+    fn from(err: ParseIntError) -> Self {
+        PathParamsError::ParseError(err)
+    }
+}
 
-    params.iter().map(|s| s.parse::<i32>()).collect()
+struct PathParams {
+    ids: Vec<i32>,
+}
+
+impl FromStr for PathParams {
+    type Err = PathParamsError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ids: Vec<i32> = s
+            .split('/')
+            .map(|num_str| num_str.parse::<i32>())
+            .collect::<Result<Vec<i32>, ParseIntError>>()?;
+
+        // check number of ids is between 1 and 20
+        if ids.len() > 20 {
+            return Err(PathParamsError::TooManyIds);
+        }
+
+        Ok(PathParams { ids })
+    }
+}
+
+impl FromRequest for PathParams {
+    type Error = Error;
+    type Future = Ready<Result<Self, Error>>;
+
+    fn from_request(req: &HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
+        let path = req.match_info().query("ids");
+
+        match PathParams::from_str(path) {
+            Ok(params) => ready(Ok(params)),
+            Err(_) => ready(Err(actix_web::error::ErrorBadRequest("Bad Request"))),
+        }
+    }
+}
+
+#[get("/1/{ids:.*}")] // match all
+async fn cube_xor_of_path_params(params: PathParams) -> String {
+    recallibrate(params.ids).to_string()
 }
 
 fn recallibrate(ids: Vec<i32>) -> i32 {
