44339b3ef20dbe26584f993f98c177705cc03071
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-07 20:45:48 +0000

    Trim down validation logic.

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -3,43 +3,9 @@ use actix_web::{Error, FromRequest, HttpRequest};
 use futures_util::future::{ready, Ready};
 use std::num::ParseIntError;
 use std::ops::BitXor;
-use std::str::FromStr;
-
-// NOTE: The boilerplate required for type-based validation is probably not
-// justified, but I want to proceed as if the domain types might be used
-// elsewhere.
-
-enum PathParamsError {
-    ParseError(ParseIntError),
-    TooManyIds,
-}
-
-impl From<ParseIntError> for PathParamsError {
-    fn from(err: ParseIntError) -> Self {
-        PathParamsError::ParseError(err)
-    }
-}
 
 struct PathParams {
     ids: Vec<i32>,
-}
-
-impl FromStr for PathParams {
-    type Err = PathParamsError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let ids: Vec<i32> = s
-            .split('/')
-            .map(|num_str| num_str.parse::<i32>())
-            .collect::<Result<Vec<i32>, ParseIntError>>()?;
-
-        // check number of ids is between 1 and 20
-        if ids.len() > 20 {
-            return Err(PathParamsError::TooManyIds);
-        }
-
-        Ok(PathParams { ids })
-    }
 }
 
 impl FromRequest for PathParams {
@@ -49,10 +15,20 @@ impl FromRequest for PathParams {
     fn from_request(req: &HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
         let path = req.match_info().query("ids");
 
-        match PathParams::from_str(path) {
-            Ok(params) => ready(Ok(params)),
-            Err(_) => ready(Err(actix_web::error::ErrorBadRequest("Bad Request"))),
+        let ids = match path
+            .split('/')
+            .map(|num_str| num_str.parse::<i32>())
+            .collect::<Result<Vec<i32>, ParseIntError>>()
+        {
+            Ok(ids) => ids,
+            Err(_) => return ready(Err(actix_web::error::ErrorBadRequest("Bad Request"))),
+        };
+
+        if ids.len() > 20 {
+            return ready(Err(actix_web::error::ErrorBadRequest("Bad Request")));
         }
+
+        ready(Ok(PathParams { ids }))
     }
 }
 
