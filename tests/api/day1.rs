139309f573b047655447a4db9c12a266fef99377
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-07 22:18:27 +0000

    Consolidates day1 tests in one.

--- tests/api/day1.rs
+++ tests/api/day1.rs
@@ -2,35 +2,15 @@ use crate::helpers::spawn_app;
 
 const DAY: u8 = 1;
 
-// Task 1
 #[tokio::test]
-async fn cube_xored_path_params() {
-    // Arrange
-    let app_address = spawn_app();
-    let client = reqwest::Client::new();
-
-    // Act
-    let response = client
-        // from example: xor(4, 8)^3 = 1728
-        .get(format!("{app_address}/{DAY}/4/8"))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    // Assert
-    assert_eq!(200, response.status().as_u16());
-    assert_eq!("1728", response.text().await.unwrap());
-}
-
-// Task 2
-#[tokio::test]
-async fn get_1_cubes_xored_path_params() {
+async fn cubes_xored_path_params() {
     // Arrange
     let app_address = spawn_app();
     let client = reqwest::Client::new();
 
     // example inputs
     let test_cases = [
+        ("4/8", 200, "1728"),     // GET /1/4/8 returns 1728
         ("10", 200, "1000"),      // GET /1/10 returns 10
         ("4/5/8/10", 200, "27"),  // GET /1/4/5/8/10 returns 27
         ("", 400, "Bad Request"), // no path params
