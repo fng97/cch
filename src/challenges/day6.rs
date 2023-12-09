f133738b91033de117beabfd9660f890c73348bb
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-09 20:27:56 +0000

    Gets day 6 task 1 passing.

--- src/challenges/day6.rs
+++ src/challenges/day6.rs
@@ -1,6 +1,14 @@
-use actix_web::post;
+use actix_web::{post, web};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Response {
+    elf: u32,
+}
 
 #[post("/6")]
-async fn count_elf_sub_strings(_body: String) -> String {
-    r#"{"elf":4}"#.to_string()
+async fn count_elf_sub_strings(body: String) -> web::Json<Response> {
+    web::Json(Response {
+        elf: body.matches("elf").count() as u32,
+    })
 }
