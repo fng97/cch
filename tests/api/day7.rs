cda6458019c872fa27e9816c500a67657d11d10e
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-09 22:31:10 +0000

    Adds failing test for day 7 task 1.

---
+++ tests/api/day7.rs
@@ -0,0 +1,28 @@
+use crate::helpers::spawn_app;
+use reqwest::Client;
+
+// Task 1
+#[tokio::test]
+async fn base64_decodes_recipe() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = Client::new();
+
+    // Act
+    let response = client
+        .get(format!("{app_address}/7/decode"))
+        .header(
+            "Cookie",
+            "recipe=eyJmbG91ciI6MTAwLCJjaG9jb2xhdGUgY2hpcHMiOjIwfQ==",
+        )
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+    assert_eq!(
+        r#"{"flour":100,"chocolate chips":20}"#,
+        response.text().await.unwrap()
+    );
+}
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 mod day4;
 mod day6;
+mod day7;
 mod helpers;
 mod warmup;
