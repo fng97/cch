72056f0492f2b2ab435944d3a2de479451aff099
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-09 00:26:10 +0000

    Adds solution to day 4 task 2.

--- src/challenges/day4.rs
+++ src/challenges/day4.rs
@@ -1,13 +1,13 @@
 use actix_web::{post, web};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-struct Reindeer {
-    strength: u8,
+struct ReindeerStats {
+    strength: u32,
 }
 
 #[post("/4/strength")]
-async fn combine_reindeer_strengths(reindeers: web::Json<Vec<Reindeer>>) -> String {
+async fn combine_reindeer_strengths(reindeers: web::Json<Vec<ReindeerStats>>) -> String {
     reindeers
         .0
         .iter()
@@ -15,15 +15,76 @@ async fn combine_reindeer_strengths(reindeers: web::Json<Vec<Reindeer>>) -> Stri
         .to_string()
 }
 
+#[derive(Deserialize)]
+struct ReindeerStatsFull {
+    name: String,
+    strength: u32,
+    speed: f32,
+    height: u32,
+    antler_width: u32,
+    snow_magic_power: u32,
+    favorite_food: String,
+    #[serde(alias = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: u32,
+}
+
+#[derive(Serialize)]
+struct Winners {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
 #[post("/4/contest")]
-async fn summarise_winners() -> String {
-    r#"
-    {
-        "fastest": "Speeding past the finish line with a strength of 5 is Dasher",
-        "tallest": "Dasher is standing tall with his 36 cm wide antlers",
-        "magician": "Dasher could blast you away with a snow magic power of 9001",
-        "consumer": "Dancer ate lots of candies, but also some grass"
+async fn summarise_winners(reindeers: web::Json<Vec<ReindeerStatsFull>>) -> web::Json<Winners> {
+    let loser: ReindeerStatsFull = ReindeerStatsFull {
+        name: "Loser".to_string(),
+        strength: 0,
+        speed: f32::MIN,
+        height: u32::MIN,
+        antler_width: 0,
+        snow_magic_power: u32::MIN,
+        favorite_food: "a humble pie".to_string(),
+        candies_eaten_yesterday: u32::MIN,
+    };
+
+    let mut fastest = &loser;
+    let mut tallest = &loser;
+    let mut magician = &loser;
+    let mut consumer = &loser;
+
+    for reindeer in reindeers.iter() {
+        if reindeer.speed > fastest.speed {
+            fastest = reindeer;
+        }
+        if reindeer.height > tallest.height {
+            tallest = reindeer;
+        }
+        if reindeer.snow_magic_power > magician.snow_magic_power {
+            magician = reindeer;
+        }
+        if reindeer.candies_eaten_yesterday > consumer.candies_eaten_yesterday {
+            consumer = reindeer;
+        }
     }
-    "#
-    .to_string()
+
+    web::Json(Winners {
+        fastest: format!(
+            "Speeding past the finish line with a strength of {} is {}",
+            fastest.strength, fastest.name
+        ),
+        tallest: format!(
+            "{} is standing tall with his {} cm wide antlers",
+            tallest.name, tallest.antler_width
+        ),
+        magician: format!(
+            "{} could blast you away with a snow magic power of {}",
+            magician.name, magician.snow_magic_power
+        ),
+        consumer: format!(
+            "{} ate lots of candies, but also some {}",
+            consumer.name, consumer.favorite_food
+        ),
+    })
 }
