c1970797f80f30534480479f753862c7a4ecb5c7
Francisco <53615823+fng97@users.noreply.github.com>
2023-12-11 23:38:04 +0000

    Fixes day 6 task 2.
    Had issues with not matching overlapping substrings.

--- src/challenges/day6.rs
+++ src/challenges/day6.rs
@@ -1,13 +1,8 @@
-use actix_web::{post, web, Either};
+use actix_web::{post, web};
 use serde::Serialize;
 
 #[derive(Serialize)]
 struct Response {
-    elf: u32,
-}
-
-#[derive(Serialize)]
-struct ResponseWithShelves {
     elf: u32,
     #[serde(rename(serialize = "elf on a shelf"))]
     elf_on_a_shelf: u32,
@@ -16,32 +11,25 @@ struct ResponseWithShelves {
 }
 
 #[post("/6")]
-async fn count_elves_and_shelves(
-    body: String,
-) -> Either<web::Json<Response>, web::Json<ResponseWithShelves>> {
-    let elf_count = count_elves(&body);
+async fn count_elves_and_shelves(body: String) -> web::Json<Response> {
+    let elf_on_a_shelf_count = count_substrs(&body, "elf on a shelf");
 
-    if !body.contains("elf on a shelf") {
-        Either::Left(web::Json(Response { elf: elf_count }))
-    } else {
-        let elves_on_shelves_count = count_elves_on_shelves(&body);
-
-        Either::Right(web::Json(ResponseWithShelves {
-            elf: elf_count,
-            elf_on_a_shelf: elves_on_shelves_count,
-            shelf_with_no_elf_on_it: count_shelves(&body) - elves_on_shelves_count,
-        }))
-    }
+    web::Json(Response {
+        elf: count_substrs(&body, "elf"),
+        elf_on_a_shelf: elf_on_a_shelf_count,
+        shelf_with_no_elf_on_it: count_substrs(&body, "shelf") - elf_on_a_shelf_count,
+    })
 }
 
-fn count_elves(text: &str) -> u32 {
-    text.matches("elf").count() as u32
-}
-
-fn count_elves_on_shelves(text: &str) -> u32 {
-    text.matches("elf on a shelf").count() as u32
-}
-
-fn count_shelves(text: &str) -> u32 {
-    text.matches("shelf").count() as u32
+/// Count the number of times the substring occurs in the text.
+///
+/// This caught me out! Must support overlapping substrings. E.g. "elf on a
+/// shelf on a shelf" should match "elf on a shelf" twice.
+/// `matches(substr).count()` would only match it once.
+fn count_substrs(text: &str, substr: &str) -> u32 {
+    text.chars()
+        .collect::<Vec<_>>()
+        .windows(substr.len()) // create sliding window of the size of the pattern
+        .filter(|window| window.iter().collect::<String>() == substr)
+        .count() as u32
 }
